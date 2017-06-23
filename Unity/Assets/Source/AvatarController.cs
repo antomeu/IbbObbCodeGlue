81dc8bddd07a6182454d9900fb1c244257456e00
antomeu <anto_meu@yahoo.fr>
2017-06-23 18:07:34 +0200

    First commit

---
+++ Unity/Assets/Source/AvatarController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarController : MonoBehaviour
+{
+    #region Set in Unity
+    public Player Player;
+    public Rigidbody Rigidbody;
+    #endregion
+    public float JumpForce = 5f;
+    public float MoveForce = 5f;
+    public float GravityDirection = 1;
+
+    void FixedUpdate()
+    {
+        float jump =0;
+        float horizontal = 0;
+        if (Player == Player.Player1)
+        {
+            jump = Input.GetAxis("Jump1");
+            horizontal = Input.GetAxis("Horizontal1");
+        }
+        if (Player == Player.Player2)
+        {
+            jump = Input.GetAxis("Jump2");
+            horizontal = Input.GetAxis("Horizontal2");
+        }
+
+        Jump(jump);
+        Walk(horizontal);
+        UpdateGravity();
+    }
+
+    private void Jump(float jump)
+    {
+        Rigidbody.AddForce(GravityDirection * jump* JumpForce * Vector3.up, ForceMode.Impulse);
+    }
+
+    private void Walk(float horizontal)
+    {
+        Rigidbody.AddForce(horizontal * MoveForce* Vector3.right);
+    }
+
+    private void UpdateGravity()
+    {
+        GravityDirection = Mathf.Sign(transform.position.y);
+        Rigidbody.AddForce(GravityDirection * Globals.Gravity * Vector3.up);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,34 @@
+The purpose of the assignment is to create basic version of the ibb & obb gameplay.
+Take a look at https://www.youtube.com/watch?v=7lcYClbSdMk for how the game plays
+
+Basic Assignment:
+-----------------
+The game should have 2 characters
+Characters can jump
+Characters don't have collision with eachother
+Characters can't fall through the floor
+Gravity below the floor is reversed
+
+Extra:
+------
+The two characters should have different heights
+Characters have collision with each other
+Characters can stand on top of each other while jumping
+
+
+We will judge the code based on the following criteria:
++ Simplicity. 
++ Clarity.
++ Object-Oriented design
++ Are there any unnecessary abstractions?
++ Are the algorithms used as simple as possible?
++ Is it easy to follow the flow of the code?
++ Is it easy to understand what the code does?
++ Do the comments really add value?
+
+This Unity project contains just the two models for ibb&obb, with the different animations already set in the importer settings.
+There is no Animator object or any code included.
+
+It also contains a simple sample scene, feel free to adjust to whatever you think is right.
+
+Feel free to send any questions to maurice@codeglue.com
