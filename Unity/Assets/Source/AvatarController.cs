21476cff40fa059dadf71b6c421a25c96d9e87d3
Antomeu <anto_meu@yahoo.fr>
2017-06-26 21:23:16 +0200

    All functionality in

--- Unity/Assets/Source/AvatarController.cs
+++ Unity/Assets/Source/AvatarController.cs
@@ -7,10 +7,14 @@ public class AvatarController : MonoBehaviour
     #region Set in Unity
     public Player Player;
     public Rigidbody Rigidbody;
+    public Collider JumpCollider;
     #endregion
     public float JumpForce = 5f;
     public float MoveForce = 5f;
+    public float MaxMoveSpeed = 2f;
+    public float AirControl = 0.1f;
     public float GravityDirection = 1;
+    private bool IsGrounded = false;
 
     void FixedUpdate()
     {
@@ -27,24 +31,54 @@ public class AvatarController : MonoBehaviour
             horizontal = Input.GetAxis("Horizontal2");
         }
 
-        Jump(jump);
-        Walk(horizontal);
+        if (jump !=0)
+            Jump(jump);
+        if (horizontal != 0)
+            Walk(horizontal);
         UpdateGravity();
+
+        RotateOnGravitySwap();
     }
 
     private void Jump(float jump)
     {
-        Rigidbody.AddForce(GravityDirection * jump* JumpForce * Vector3.up, ForceMode.Impulse);
+        if (IsGrounded)
+            Rigidbody.AddForce(GravityDirection * jump* JumpForce * Vector3.up, ForceMode.VelocityChange);
     }
 
     private void Walk(float horizontal)
     {
-        Rigidbody.AddForce(horizontal * MoveForce* Vector3.right);
+        if (Mathf.Abs(Rigidbody.velocity.x) <= MaxMoveSpeed)
+        {
+            if (IsGrounded)
+                Rigidbody.AddForce(horizontal * MoveForce * Vector3.right);
+            else
+                Rigidbody.AddForce(horizontal * AirControl * MoveForce * Vector3.right);
+        }
     }
 
     private void UpdateGravity()
     {
         GravityDirection = Mathf.Sign(transform.position.y);
         Rigidbody.AddForce(GravityDirection * Globals.Gravity * Vector3.up);
+    }
+
+    private void RotateOnGravitySwap()
+    {
+        if (GravityDirection == 1)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(transform.forward, Vector3.up), 10f);
+        if (GravityDirection == -1)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(transform.forward, Vector3.down), 10f);
+    }
+
+
+    private void OnTriggerEnter()
+    {
+        IsGrounded = true;
+    }
+
+    private void OnTriggerExit()
+    {
+        IsGrounded = false;
     }
 }
